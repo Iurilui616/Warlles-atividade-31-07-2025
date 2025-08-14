f370d2093dfae1ea3d166f50a297f1a3b313719c
Iuri Luis <iuriluis1864@gmail.com>
2025-08-14 10:47:15 -0300

    Update Questão 2.c

--- Questão 2.c
+++ Questão 2.c
@@ -5,7 +5,7 @@ int main() {
 
     printf("Numero\tQuadrado\tCubo\n");
     for (i = 0; i <= 10; i++) {
-        printf("%d\t%d\t\t%d\n", i, i * i, i * i * i);
+        printf("%d\t%8d\t%8d\n", i, i * i, i * i * i);
     }
 
     return 0;
